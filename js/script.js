ba402241e834a42571652ee06adb0f87ab815fe8
cliffnyendwe"git config --global user.name "cliffnyendwe"git commit -m"add fix indentation error"git status <cliffnyendwe>
2018-09-11 08:41:07 +0300

    added html js and md files

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var add=function (number1,number2) {
+  return number1 + number2;
+}
+undefined
+var subtract=function subtract(number1, number2) {
+return number1 - number2;
+}; alert 		(subtract(10,5));
+undefined
+var subtract=function subtract(number1, number2) {
+return number1 - number2;
+};																 var number1 =prompt("enter a number");					 alert(number1);
+undefined
